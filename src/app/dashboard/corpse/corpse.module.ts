133e458fe8b0fbb5a200a877269deb0453b49618
Yazri <yazrip@gmail.com>
2021-12-05 12:43:09 +0700

    [YZR][RG-022-FE] Reservation

--- src/app/dashboard/corpse/corpse.module.ts
+++ src/app/dashboard/corpse/corpse.module.ts
@@ -3,15 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { CorpseRoutingModule } from './corpse-routing.module';
 import { CorpseComponent } from './corpse.component';
-import { CorpseFormComponent } from './corpse-form/corpse-form.component';
-import { CorpseListComponent } from './corpse-list/corpse-list.component';
+
 
 
 @NgModule({
   declarations: [
     CorpseComponent,
-    CorpseFormComponent,
-    CorpseListComponent
   ],
   imports: [
     CommonModule,
